1472a8968cf6fa23a6e161e3646a6238b612e6b9
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-10-29 19:41:25 +0530

    Update autoleave.py

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -10,10 +10,8 @@ from DAXXMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(
-            config.AUTO_LEAVE_ASSISTANT_TIME
-        ):
+     if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
             from DAXXMUSIC.core.userbot import assistants
 
             for num in assistants:
