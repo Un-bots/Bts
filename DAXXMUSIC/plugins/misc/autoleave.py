6f18ea0455367a1123e5d0ec54ef2463408f4d52
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-10-30 03:13:54 +0530

    Update autoleave.py

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -10,30 +10,36 @@ from DAXXMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-     if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(900):
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
             from DAXXMUSIC.core.userbot import assistants
 
-                        for num in assistants:
+            for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            "supergroup",
+                            "group",
+                            "channel",
                         ]:
+                            chat_id = i.chat.id
                             if (
-                                i.chat.id != config.LOGGER_ID
+                                chat_id != config.LOGGER_ID
                                 and i.chat.id != -1001928799341
                                 and i.chat.id != -1001928799341
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(i.chat.id):
+                                if not await is_active_chat(chat_id):
                                     try:
-                                        await client.leave_chat(i.chat.id)
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
                                         left += 1
                                     except:
                                         continue
